2bbb4529a08eadeb52893bd6ecfd445c16f83a1a
VeerpalB <VeerpalB@users.noreply.github.com>
2015-10-11 20:34:06 -0400

    work towards adding button
    bold, italic, underline. Code not working like i want it too.

--- script.js
+++ script.js
@@ -1,4 +1,37 @@
 
+function doRichEditCommand(aName, aArg){
+  getIFrameDocument('editor').execCommand(aName,false, aArg);
+  document.getElementById('editor').contentWindow.focus()
+}
+
 function makeEditable(divName) {
       window[divName + '_msg'].document.designMode = 'On';
   }
+
+  function doRich(){
+    var iframe = document.getElementById('my-editable');
+    var iframeDoc = iframe.contentWindow.document;
+    var iframeBody=iframeDoc.body
+
+    // type and select some text into iframe
+    // For making selected text to be bold, just execute the following command
+    document.getElementById("editor").contentWindow.document.body.execCommand("bold", false, null);
+  }
+
+function iBold(){
+  editor.document.execCommand('bold',false,null);
+}
+function iFrameOn(){
+    editor.document.designMode = 'On';
+}
+
+function iUnderline(){
+    editor.document.execCommand('underline', false, null);
+}
+function iItalic(){
+    editor.document.execCommand('italic', false, null);
+}
+
+function code(){
+    editor.document.execCommand('insertHTML', false, '<div><code> insert code here </code></div>');
+}
