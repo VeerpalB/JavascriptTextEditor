655dbb89940626fcc340673811b42db074d037fb
VeerpalB <VeerpalB@users.noreply.github.com>
2015-10-28 18:59:10 -0400

    Added button to surround selected text with code tags
    You can now add "code" in the text editor, using the C button.

--- script.js
+++ script.js
@@ -1,37 +1,34 @@
-
-function doRichEditCommand(aName, aArg){
-  getIFrameDocument('editor').execCommand(aName,false, aArg);
-  document.getElementById('editor').contentWindow.focus()
-}
-
-function makeEditable(divName) {
-      window[divName + '_msg'].document.designMode = 'On';
-  }
-
-  function doRich(){
-    var iframe = document.getElementById('my-editable');
-    var iframeDoc = iframe.contentWindow.document;
-    var iframeBody=iframeDoc.body
-
-    // type and select some text into iframe
-    // For making selected text to be bold, just execute the following command
-    document.getElementById("editor").contentWindow.document.body.execCommand("bold", false, null);
-  }
-
 function iBold(){
   editor.document.execCommand('bold',false,null);
+  editor.focus()
 }
 function iFrameOn(){
     editor.document.designMode = 'On';
+    editor.focus()
 }
 
 function iUnderline(){
     editor.document.execCommand('underline', false, null);
+    editor.focus()
 }
 function iItalic(){
     editor.document.execCommand('italic', false, null);
+    editor.focus()
 }
 
-function code(){
-    editor.document.execCommand('insertHTML', false, '<div><code> insert code here </code></div>');
+function iCode(){
+  var iframe = document.getElementById('editor');
+var innerDoc = editor.contentDocument || iframe.contentWindow.document;
+  var code = innerDoc.createElement("code");
+
+  if (innerDoc.getSelection) {
+      var sel = innerDoc.getSelection();
+      if (sel.rangeCount) {
+          var range = sel.getRangeAt(0).cloneRange();
+          range.surroundContents(code);
+          sel.removeAllRanges();
+          sel.addRange(range);
+      }
+  }
+  editor.focus()
 }
