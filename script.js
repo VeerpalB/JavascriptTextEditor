a2f08bec7066cbe639c6539d9363d3b344d04d12
VeerpalB <VeerpalB@users.noreply.github.com>
2015-12-11 18:21:48 -0500

    Added basic local storage capabilities
    both the text area and text editor can be saved to text area, but user
    is required to click to seprate save buttons.

--- script.js
+++ script.js
@@ -1,94 +1,28 @@
 function iBold(){
   editor.document.execCommand('bold',false,null);
-  editor.focus()
-}
+  editor.focus();}
 function iFrameOn(){
     editor.document.designMode = 'On';
-    editor.focus()
+    if (localStorage["editor"] !== null) {
+      editor.document.open('text/html');
+      editor.document.write(localStorage["editor"]);
+      editor.document.close();};
+    editor.focus();
 }
 
 function iUnderline(){
     editor.document.execCommand('underline', false, null);
-    editor.focus()
+    editor.focus();
 }
 function iItalic(){
     editor.document.execCommand('italic', false, null);
-    editor.focus()
+    editor.focus();
 }
-
-// function iCode(){
-//       editor.document.open();
-//       editor.write("<textarea id="demo"></textarea>");
-//       // editor.write("var editor = new Behave({\
-//       //
-//       //   textarea: 		document.getElementById('demo'),\
-//       //   replaceTab: 	true,\
-//       //     softTabs: 		true,\
-//       //     tabSize: 		4,\
-//       //     autoOpen: 		true,\
-//       //     overwrite: 		true,\
-//       //     autoStrip: 		true,\
-//       //     autoIndent: 	true\
-//       // });");
-//       // var editor = new Behave({
-//       //
-//       //   textarea: 		editor.document.getElementById('demo'),
-//       //   replaceTab: 	true,
-//       //     softTabs: 		true,
-//       //     tabSize: 		4,
-//       //     autoOpen: 		true,
-//       //     overwrite: 		true,
-//       //     autoStrip: 		true,
-//       //     autoIndent: 	true
-//       // });
-//       editor.document.close();
-//
-//
-//     }
-
-
-//
-// function iCodeUndo(){
-//   var iframe = document.getElementById('editor');
-//   var innerDoc = editor.contentDocument || iframe.contentWindow.document;
-//   var code = innerDoc.createElement("code");
-//   code.className = "inserted_code"
-//
-//   if (innerDoc.getSelection) {
-//       var sel = innerDoc.getSelection();
-//       if (sel.rangeCount) {
-//           var range = sel.getRangeAt(0).cloneRange();
-//           range= range.replace(/<code>/gi, "");
-//           range= range.replace(/<\/code>/gi, "");
-//           surround=False;
-//           sel.removeAllRanges();
-//           sel.addRange(range);
-//
-//       }
-//   }
-//   editor.focus()
-// }
-//
-// function insertHTML() {
-//   var iframe = document.getElementById('editor');
-//   var innerDoc = editor.contentDocument || iframe.contentWindow.document;
-//   var code = innerDoc.createElement("code");
-//   code.className = "inserted_code"
-//
-//   if (innerDoc.getSelection) {
-//       var sel = innerDoc.getSelection();
-//       if (sel.rangeCount) {
-//         range = sel.getRangeAt(0);
-//         range.collapse(true);
-//         var span = document.createElement("code");
-//         span.id = "myId";
-//         span.appendChild( document.createTextNode("insert code here") );
-//         range.insertNode(span);
-//
-//         // Move the caret immediately after the inserted span
-//         range.setStartAfter(span);
-//         range.collapse(true);
-//         sel.removeAllRanges();
-//         sel.addRange(range);
-//     }
-// }
+function iSave(){
+    // return document object or window object
+    var iframe = $("#editor")[0];
+    var iframeDoc = iframe.contentDocument || iframe.contentWindow.document;
+    var val = iframeDoc.body.innerHTML;
+    window.localStorage.setItem("editor",val);
+    editor.focus();
+}
--- script_text.js
+++ script_text.js
@@ -1,7 +1,12 @@
 window.addEventListener("load", function() {
+  BehaveHooks.add(['init:after'], function(data){
+     if (localStorage["code"] !== null) {
+       document.getElementById('code').value =localStorage["code"];
+     };
+     });
 var editor = new Behave({
 
-  textarea: 		document.getElementById('demo'),
+  textarea: 		document.getElementById('code'),
   replaceTab: 	true,
     softTabs: 		true,
     tabSize: 		4,
@@ -12,4 +17,11 @@ var editor = new Behave({
     fence: false
 });
 
+
 });
+
+function Save(){
+    var text = document.getElementById('code').value;
+    window.localStorage.setItem("code",text);
+    editor.focus();
+}
